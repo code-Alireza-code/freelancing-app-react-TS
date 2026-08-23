ab21572c4de922e3ab1815b2552dd12b4de16021
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2026-08-23 09:26:16 +0330

    feat(web): add path alias and some packages

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,7 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+    tsconfigPaths(),
   ],
   server: {
     port: 3000,
