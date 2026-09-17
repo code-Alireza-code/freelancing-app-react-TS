e73cc3dbffacb9a30f2591ed3f2c5ec5e75ff7fd
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2026-09-17 10:13:44 +0330

    perf(web): add loading state to OwnerDashboard and fix typescript error of projects

--- apps/web/src/features/owner/components/OwnerDashboard.tsx
+++ apps/web/src/features/owner/components/OwnerDashboard.tsx
@@ -1,9 +1,12 @@
 import DashboardHeader from "@/features/owner/components/DashboardHeader";
 import { useGetOwnerProjects } from "@/features/projects/queries/projectQueries";
 import Stats from "./Stats";
+import { Loading } from "@/components/Loading";
 
 export default function OwnerDashboard() {
-  const { projects } = useGetOwnerProjects();
+  const { projects = [], isLoadingProjects } = useGetOwnerProjects();
+
+  if (isLoadingProjects) return <Loading />;
   return (
     <div>
       <DashboardHeader />
